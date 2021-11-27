438cf27c15d3a5b40b3ada67c12399292cff0012
lokified <sheldon.obonyo@student.moringaschool.com>
2021-11-27 12:21:24 +0300

    add event to the button

--- app/src/main/java/com/loki/yourpet/MainActivity.java
+++ app/src/main/java/com/loki/yourpet/MainActivity.java
@@ -2,13 +2,32 @@ package com.loki.yourpet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @BindView(R.id.welcomeButton) Button welcomeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        welcomeButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == welcomeButton) {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+        }
     }
 }
