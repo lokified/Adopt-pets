7866c5f955f388dae5ccf98ed47f2131e9e530c6
lokified <sheldon.obonyo@student.moringaschool.com>
2021-11-27 15:03:00 +0300

    add method to login button

--- app/src/main/java/com/loki/yourpet/LoginActivity.java
+++ app/src/main/java/com/loki/yourpet/LoginActivity.java
@@ -2,14 +2,34 @@ package com.loki.yourpet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class LoginActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @BindView(R.id.loginButton) Button mLoginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
 
+        mLoginButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mLoginButton) {
+            Intent intent = new Intent(LoginActivity.this,PetListActivity.class);
+            startActivity(intent);
+        }
     }
 }
