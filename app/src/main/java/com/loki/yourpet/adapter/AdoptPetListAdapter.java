7d6d28930682dc2fcda2b2943438290789ad03dc
lokified <sheldon.obonyo@student.moringaschool.com>
2021-12-04 16:33:13 +0300

    add a custom adapter to list the pets

---
+++ app/src/main/java/com/loki/yourpet/adapter/AdoptPetListAdapter.java
@@ -0,0 +1,73 @@
+package com.loki.yourpet.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.loki.yourpet.R;
+import com.loki.yourpet.models.Animal;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class AdoptPetListAdapter extends RecyclerView.Adapter<AdoptPetListAdapter.PetViewHolder> {
+
+    private List<Animal> mAnimals;
+    private Context mContext;
+
+    public AdoptPetListAdapter(Context context, List<Animal> animals) {
+        mContext = context;
+        mAnimals = animals;
+    }
+
+    @Override
+    public AdoptPetListAdapter.PetViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.adopt_pet_list,parent,false);
+        PetViewHolder viewHolder = new PetViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(AdoptPetListAdapter.PetViewHolder holder, int position) {
+        holder.bindAnimal(mAnimals.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mAnimals.size();
+    }
+
+    public class PetViewHolder extends RecyclerView.ViewHolder {
+
+        @BindView(R.id.petImageView)
+        ImageView mPetImage;
+        @BindView(R.id.petName)
+        TextView mPetName;
+        @BindView(R.id.descriptionName) TextView mPetDescription;
+
+        private Context mContext;
+
+        public PetViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this,itemView);
+            mContext = itemView.getContext();
+
+        }
+
+        public void bindAnimal(Animal animal) {
+            Picasso.get().load(animal.getPhotos().get(0).getMedium()).into(mPetImage);
+            mPetName.setText(animal.getName());
+            mPetDescription.setText(animal.getDescription());
+        }
+
+
+    }
+}
