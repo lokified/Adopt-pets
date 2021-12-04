628349df871a741bafb7d5faf5a5e26dfc8fc0a0
lokified <sheldon.obonyo@student.moringaschool.com>
2021-12-04 16:16:11 +0300

    create constant class

---
+++ app/src/main/java/com/loki/yourpet/Constants.java
@@ -0,0 +1,7 @@
+package com.loki.yourpet;
+
+public class Constants {
+
+    public static final String PET_BASE_URL = "https://api.petfinder.com/v2/";
+    public static final String  PET_ACCESS_TOKEN = BuildConfig.PET_ACCESS_TOKEN;
+}
