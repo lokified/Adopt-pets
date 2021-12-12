910a66ccb61254a4308dc6bfb256e8f93d2661dc
lokified <sheldon.obonyo@student.moringaschool.com>
2021-12-12 22:14:00 +0300

    authenticate the user login

--- app/src/main/java/com/loki/yourpet/ui/LoginActivity.java
+++ app/src/main/java/com/loki/yourpet/ui/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.loki.yourpet.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,6 +11,11 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.loki.yourpet.R;
 
 import butterknife.BindView;
@@ -17,18 +23,36 @@ import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
     @BindView(R.id.loginButton) Button mLoginButton;
     @BindView(R.id.editTextPersonEmail) EditText mPersonEmail;
     @BindView(R.id.editTextPersonPassword) EditText mPersonPassword;
     @BindView(R.id.registerTextView) TextView mRegisterTextView;
-
-    Boolean isAllFieldsChecked = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        //authenticate user log in
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                if (user != null) {
+                    Intent intent = new Intent(LoginActivity.this, PetListActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
 
         mLoginButton.setOnClickListener(this);
         mRegisterTextView.setOnClickListener(this);
@@ -37,11 +61,10 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     @Override
     public void onClick(View v) {
-        isAllFieldsChecked = checkAllFields();
-
         //logs in user
-        if(v == mLoginButton && isAllFieldsChecked) {
+        if(v == mLoginButton) {
             Intent intent = new Intent(LoginActivity.this, PetListActivity.class);
+            loginWithPassword();
             Toast.makeText(getApplicationContext(),"Login successfully",Toast.LENGTH_SHORT).show();
             startActivity(intent);
         }
@@ -55,17 +78,46 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     }
 
-    //validate the login form
-    private Boolean checkAllFields() {
+    public void loginWithPassword() {
+        //validate the login form
 
-        if(mPersonEmail.length() == 0 ) {
+        String email = mPersonEmail.getText().toString();
+        String password = mPersonPassword.getText().toString();
+
+        if(email.equals("")) {
             mPersonEmail.setError("please input your email");
-            return false;
+            return ;
         }
-        if (mPersonPassword.length() == 0 ) {
+        if (password.equals("")) {
             mPersonPassword.setError("please enter your password");
-            return false;
+            return ;
         }
-        return true;
+
+        //listens from firebase
+        mAuth.signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (!task.isSuccessful()) {
+
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mAuth.removeAuthStateListener(mAuthListener);
     }
 }
