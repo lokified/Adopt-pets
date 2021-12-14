200f3e7d58852953c40668774d333f7c3a1bdaea
lokified <sheldon.obonyo@student.moringaschool.com>
2021-12-14 08:39:44 +0300

    make changes

--- app/src/main/java/com/loki/yourpet/ui/CreateAccountActivity.java
+++ app/src/main/java/com/loki/yourpet/ui/CreateAccountActivity.java
@@ -131,7 +131,7 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
                 final FirebaseUser user = firebaseAuth.getCurrentUser();
 
                 if (user != null) {
-                    Intent intent = new Intent(CreateAccountActivity.this, MainActivity.class);
+                    Intent intent = new Intent(CreateAccountActivity.this, LoginActivity.class);
                     intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                     startActivity(intent);
                     finish();
